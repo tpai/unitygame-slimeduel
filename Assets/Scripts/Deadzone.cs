482e7f7973e9c299fbe9dc9a96a31e1a360f74d8
tonypai <tony77794@gmail.com>
2015-09-03 00:56:13 +0800

    修復P3,P4 Deadzone無效之問題

--- Assets/Scripts/Deadzone.cs
+++ Assets/Scripts/Deadzone.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Deadzone : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.tag == "P1" || other.tag == "P2") {
+		if (other.tag == "P1" || other.tag == "P2" || other.tag == "P3" || other.tag == "P4") {
 			GameObject.Find ("HUD/"+other.tag+"/Life").GetComponent<Life>().Lost ();
 			GetComponent<AudioSource> ().Play ();
 		}
