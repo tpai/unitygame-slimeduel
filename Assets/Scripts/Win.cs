3357e5d652c7457a3177030750c91091b037d9c4
Tonypai <tony77794@gmail.com>
2015-11-15 02:21:25 +0800

    Fix wrong particle in muscus bomb and wait 3s then restart when entering end scene

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -5,16 +5,22 @@ using System.Collections;
 public class Win : MonoBehaviour {
 
 	public Sprite[] sprites;
+	bool freeToRestart = false;
 
 	void Start () {
 		Destroy (GameObject.Find ("UI"));
 		int winner = int.Parse (PlayerPrefs.GetString ("Winner").Replace ("P", ""));
-		Debug.Log (winner);
 		GetComponent<Image> ().sprite = sprites [winner-1];
+		StartCoroutine (Wait ());
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds (3f);
+		freeToRestart = true;
 	}
 
 	void Update () {
-		if (Input.anyKeyDown) {
+		if (freeToRestart && Input.anyKeyDown) {
 			Application.LoadLevel ("Menu");
 		}
 	}
